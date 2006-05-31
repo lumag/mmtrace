484ab3a3e45a336394f487766cd2997d3ff62363
Dmitry Baryshkov <dbaryshkov@gmail.com>
2006-05-31 12:49:52 +0400

    Instrument stores: I8, I16, I32, I64, F64. F32 requires Iop_ReinterpF32asI32 which doesn't work. I128, V128 should be easy to add if necessary.

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CFLAGS = -I/usr/include/valgrind \
 	 -DVGA_x86=1 \
 	 -DVGO_linux=1 \
 	 -DVGP_x86_linux=1 \
-	 -g -Wall -Wno-pointer-sign
+	 -g -Wall -Wno-pointer-sign -Werror
 LDFLAGS = -L/usr/lib/valgrind/x86-linux -lcoregrind -lvex -lgcc \
 	-static -Wl,-defsym,valt_load_address=0xa0000000 \
 	-nodefaultlibs -nostartfiles -u _start \
--- mt_main.c
+++ mt_main.c
@@ -6,16 +6,113 @@
 #include "pub_tool_mallocfree.h"
 #include "pub_tool_tooliface.h"
 
+static void mt_store_1(ULong *addr, UInt data) {
+//	VG_(message)(Vg_UserMsg, "here: [%08x] = %02x!", addr, (UChar)data);
+}
+
+static void mt_store_2(ULong *addr, UInt data) {
+//	VG_(message)(Vg_UserMsg, "here: [%08x] = %04x!", addr, (UShort)data);
+}
+
+static void mt_store_4(ULong *addr, UInt data) {
+//	VG_(message)(Vg_UserMsg, "here: [%08x] = %08x!", addr, data);
+}
+
+static void mt_store_8(ULong *addr, ULong data) {
+//	VG_(message)(Vg_UserMsg, "here: [%08x] = %016llx!", addr, data);
+}
+
 static void mt_post_clo_init(void)
 {
 }
 
 static
-IRBB* mt_instrument(IRBB* bb, VexGuestLayout* layout, 
+IRBB* mt_instrument(IRBB* bb_in, VexGuestLayout* layout, 
                     Addr64 orig_addr_noredir, VexGuestExtents* vge,
                     IRType gWordTy, IRType hWordTy)
 {
-    return bb;
+	IRBB *bb;
+	int i;
+
+	if (gWordTy != hWordTy) {
+		/* We don't currently support this case. */
+		VG_(tool_panic)("host/guest word size mismatch");
+	}
+
+	/*
+	 * Set up BB
+	 */
+	bb = emptyIRBB();
+	bb->tyenv = dopyIRTypeEnv(bb_in->tyenv);
+	bb->next = dopyIRExpr(bb_in->next);
+	bb->jumpkind = bb_in->jumpkind;
+
+	for (i = 0; i < bb_in->stmts_used; i++) {
+		IRStmt *st = bb_in->stmts[i];
+		IRDirty *di;
+		IRTemp temp;
+
+		switch (st->tag) {
+			case Ist_Store:
+				{
+					di = NULL;
+					IRType type = typeOfIRExpr(bb->tyenv, st->Ist.Store.data);
+					switch (type) {
+						case Ity_I8:
+							temp = newIRTemp(bb->tyenv, Ity_I32);
+							addStmtToIRBB(bb, IRStmt_Tmp(temp,  IRExpr_Unop(Iop_8Uto32, st->Ist.Store.data)));
+							di = unsafeIRDirty_0_N (0, "mt_store_1",
+									&mt_store_1, mkIRExprVec_2(st->Ist.Store.addr, IRExpr_Tmp(temp)));
+							break;
+						case Ity_I16:
+							temp = newIRTemp(bb->tyenv, Ity_I32);
+							addStmtToIRBB(bb, IRStmt_Tmp(temp,  IRExpr_Unop(Iop_16Uto32, st->Ist.Store.data)));
+							di = unsafeIRDirty_0_N (0, "mt_store_2",
+									&mt_store_2, mkIRExprVec_2(st->Ist.Store.addr, IRExpr_Tmp(temp)));
+							break;
+						case Ity_I32:
+							di = unsafeIRDirty_0_N (0, "mt_store_4",
+									&mt_store_4, mkIRExprVec_2(st->Ist.Store.addr, st->Ist.Store.data));
+							break;
+						case Ity_I64:
+							di = unsafeIRDirty_0_N (0, "mt_store_8",
+									&mt_store_8, mkIRExprVec_2(st->Ist.Store.addr, st->Ist.Store.data));
+							break;
+ 						case Ity_F32:
+/*
+ * FIXME: enable when Iop_ReinterpF32asI32 is supported in the VEX
+							temp = newIRTemp(bb->tyenv, Ity_I32);
+							addStmtToIRBB(bb, IRStmt_Tmp(temp,  IRExpr_Unop(Iop_ReinterpF32asI32, st->Ist.Store.data)));
+							di = unsafeIRDirty_0_N (0, "mt_store_4",
+									&mt_store_4, mkIRExprVec_2(st->Ist.Store.addr, IRExpr_Tmp(temp)));
+									*/
+							break;
+ 						case Ity_F64:
+							temp = newIRTemp(bb->tyenv, Ity_I64);
+							addStmtToIRBB(bb, IRStmt_Tmp(temp,  IRExpr_Unop(Iop_ReinterpF64asI64, st->Ist.Store.data)));
+							di = unsafeIRDirty_0_N (0, "mt_store_8",
+									&mt_store_8, mkIRExprVec_2(st->Ist.Store.addr, IRExpr_Tmp(temp)));
+							break;
+						default:
+							{
+								Char *buf = VG_(malloc)(128);
+								VG_(snprintf)(buf, 128, "unhandled type: %x!", type);
+								VG_(tool_panic)(buf);
+							}
+							break;
+					}
+					if (di != NULL) {
+						addStmtToIRBB(bb, IRStmt_Dirty(di));
+					}
+					addStmtToIRBB(bb, st);
+				}
+			default:
+				addStmtToIRBB(bb, st);
+				break;
+		}
+	}
+
+	return bb;
 }
 
 static void mt_fini(Int exitcode)
@@ -25,16 +122,18 @@ static void mt_fini(Int exitcode)
 static
 void mt_new_mem_mmap ( Addr a, SizeT len, Bool rr, Bool ww, Bool xx )
 {
+	Char *name = NULL;
 	NSegment* seg = VG_(am_find_nsegment) (a);
-	if (seg->kind != SkFileC) {
-		return;
-	}
-	Char *name;
-	{
+	if (seg->kind == SkFileC) {
 		HChar *hname = VG_(am_get_filename) (seg);
 		tl_assert(hname != NULL);
 		name = VG_(malloc)(VG_(strlen)(hname));
 		VG_(strcpy)(name, hname);
+	}
+#if 0
+	else
+	{
+		return;
 	}
 	if (VG_(strncmp)("/dev/", name, 5)) {
 		return;
@@ -42,18 +141,21 @@ void mt_new_mem_mmap ( Addr a, SizeT len, Bool rr, Bool ww, Bool xx )
 	if (!VG_(strcmp)("/dev/zero", name)) {
 		return;
 	}
-	VG_(printf)("mmap: %s %x of size %x (%s%s%s)\n", name, a, len,
+#endif
+	VG_(message)(Vg_DebugMsg, "mmap: %s at %x size %x (%s%s%s)", name, a, len,
 			rr?"r":"-",
 			ww?"w":"-",
 			xx?"x":"-"
 			);
 
-	VG_(free)(name);
+	if (name != NULL) {
+		VG_(free)(name);
+	}
 }
 
 static
 void mt_die_mem_munmap ( Addr a, SizeT len ) {
-	VG_(printf)("munmap: %x of size %x\n", a, len);
+	VG_(message)(Vg_DebugMsg, "munmap: %x of size %x", a, len);
 }
 
 static void mt_pre_clo_init(void)
