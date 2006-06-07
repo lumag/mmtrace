825cff302f7d1ff28ab6027740bc2dfb019d2abe
Dmitry Baryshkov <dbaryshkov@gmail.com>
2006-06-08 02:53:13 +0400

    Replace cmd's with object + offset.

--- mt_nvidia_fifo.h
+++ mt_nvidia_fifo.h
@@ -1,10 +1,18 @@
-static ULong fifo_cmdstart = 0;
+typedef struct {
+	UInt name;
+	UInt type;
+} fifo_descriptor;
+
+static fifo_descriptor fifo_channels[8];
+
 static ULong fifo_expected = 0;
 static int fifo_nops = 0;
 
+static int fifo_cmdstart = 0;
 static int fifo_cmdlen = 0;
+
 static int fifo_channel = 0;
-static int fifo_cmd = 0;
+static int fifo_object_offset = 0;
 
 static void fifo_flush() {
 	if (fifo_nops != 0) {
@@ -33,7 +41,7 @@ static void fifo_store_4(Char *name, ULong offset, UInt data) {
 
 		fifo_cmdlen = (data & 0x1ffc0000) >> 18;
 		fifo_channel = (data & 0x0000e000) >> 13;
-		fifo_cmd = data & 0x1FFF;
+		fifo_object_offset = data & 0x1FFF;
 
 		if (data == 0) {
 			if (offset != fifo_expected) {
@@ -46,14 +54,20 @@ static void fifo_store_4(Char *name, ULong offset, UInt data) {
 		} else {
 			ML_(trace_flush)();
 
-			pos += VG_(snprintf)(buf+pos, BUFSIZ-pos, "  {size: 0x%-3x   channel: 0x%-1x   cmd:   ", fifo_cmdlen, fifo_channel);
-			// FIXME: cmd name
-			pos += VG_(snprintf)(buf+pos, BUFSIZ-pos, "0x%04x", fifo_cmd);
+			pos += VG_(snprintf)(buf+pos, BUFSIZ-pos, "  {size: 0x%-3x   channel: 0x%-1x}", fifo_cmdlen, fifo_channel);
 
 			fifo_expected = offset + 4;
 		}
 	} else {
-		pos += VG_(snprintf)(buf+pos, BUFSIZ-pos, "  cmd=0x%08x, arg=%lld", fifo_cmd, (offset - fifo_cmdstart)>>2);
+		if (fifo_object_offset == 0) {
+			fifo_channels[fifo_channel].name = data;
+			// FIXME!
+			fifo_channels[fifo_channel].type = (data & 0x0000ff00) >> 8;
+		}
+		pos += VG_(snprintf)(buf+pos, BUFSIZ-pos, "  object: %08x type=%02x  offset=%04x",
+				fifo_channels[fifo_channel].name,
+				fifo_channels[fifo_channel].type,
+				fifo_object_offset);
 	}
 
 	VG_(message)(Vg_UserMsg, buf);
